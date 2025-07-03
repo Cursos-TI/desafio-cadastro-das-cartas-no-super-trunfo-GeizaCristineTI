6828c85799e71e45c25d2d80cfc2ea720848dc67
Geiza Cristine <geizacristineti@gmail.com>
2025-07-03 02:15:21 +0000

    criei as cartas # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   CartasSuperTrunfo.c #

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,10 +15,20 @@ int main() {
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int  codigo da Cidade=22;
+    char nome da Cidade[50]= Fortaleza; 
+    int populacao = 2687000; // População estimada
+    float area = 313.0; // Área em km²
+    float pib = 1000000000.0; // PIB em reais
+    int pontosTuristicos = 10; // Número de pontos turísticos
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Digite o código da cidade: ");
+    scanf("%d", &codigo_da_Cidade);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome_da_Cidade);
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
